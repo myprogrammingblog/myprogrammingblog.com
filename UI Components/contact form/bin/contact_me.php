0d5e2367a99478e46a59f1f74f4a6350a806e852
Anatoly Spektor <myprogrammingblog@gmail.com>
2013-08-27 12:16:20 -0400

    Added Contact form

---
+++ UI Components/contact form/bin/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// check if fields passed are empyu
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'me@myprogrammingblog.com'; // put your email
+$email_subject = "Contact form submitted by:  $name";
+$email_body = "You have received a new message. \n\n".
+				  " Here are the details:\n \nName: $name \n ".
+				  "Email: $email_address\n Message \n $message";
+$headers = "From: contacts@anatolyspektor.com\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
