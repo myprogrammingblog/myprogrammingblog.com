25803a543fa1de27e1531825df617a9d449d6487
Anatoly Spektor <myprogrammingblog@gmail.com>
2016-08-10 20:40:06 -0700

    Specify ActionCable mount route and create notification streaming channel

--- Ruby_Scripts/notificator/app/channels/notifications_channel.rb
+++ Ruby_Scripts/notificator/app/channels/notifications_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "notification_channel"
   end
 
   def unsubscribed
