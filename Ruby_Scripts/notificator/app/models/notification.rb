59b0c409d9a1e4ca449f0bcb4d07db9228dbf81a
Anatoly Spektor <myprogrammingblog@gmail.com>
2016-08-10 21:06:32 -0700

    Passing notification to job via after_commit_create hook

--- Ruby_Scripts/notificator/app/models/notification.rb
+++ Ruby_Scripts/notificator/app/models/notification.rb
@@ -1,3 +1,3 @@
 class Notification < ApplicationRecord
-  after_create_commit { NotificationBroadcastJob.perform_later(Notification.count)}
+  after_create_commit { NotificationBroadcastJob.perform_later(Notification.count,self)}
 end
