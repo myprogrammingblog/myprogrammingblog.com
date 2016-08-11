4c390ddbe8ac54589b54b0a78434c797be4491a8
Anatoly Spektor <myprogrammingblog@gmail.com>
2016-08-10 20:01:35 -0700

    Allow request from localhosti n development to open a notification stream

--- Ruby_Scripts/notificator/config/environments/development.rb
+++ Ruby_Scripts/notificator/config/environments/development.rb
@@ -45,6 +45,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_cable.allowed_request_origins = ['http://0.0.0.0:3222', 'http://0.0.0.0:3000', 'http://localhost:3000']
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
