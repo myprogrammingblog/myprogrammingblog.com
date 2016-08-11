08979f51f342fce7598828661ee41d4829cf495a
Anatoly Spektor <myprogrammingblog@gmail.com>
2016-08-10 20:26:39 -0700

    Make NotificationJob to broadcast counter value via ActionCable using counter partial

--- Ruby_Scripts/notificator/app/jobs/notification_broadcast_job.rb
+++ Ruby_Scripts/notificator/app/jobs/notification_broadcast_job.rb
@@ -1,7 +1,13 @@
 class NotificationBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(counter)
+    ActionCable.server.broadcast 'notification_channel',  counter: render_counter(counter)
+  end
+
+  private
+
+  def render_counter(counter)
+    ApplicationController.renderer.render(partial: 'notifications/counter', locals: { counter: counter })
   end
 end
