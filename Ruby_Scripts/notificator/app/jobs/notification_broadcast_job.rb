080aa43018de331a685fb55b52a64d25d2a3eac1
Anatoly Spektor <myprogrammingblog@gmail.com>
2016-08-10 21:06:08 -0700

    Updating Notification Broadcast job to render notification

--- Ruby_Scripts/notificator/app/jobs/notification_broadcast_job.rb
+++ Ruby_Scripts/notificator/app/jobs/notification_broadcast_job.rb
@@ -1,13 +1,17 @@
 class NotificationBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(counter)
-    ActionCable.server.broadcast 'notification_channel',  counter: render_counter(counter)
+  def perform(counter,notification)
+    ActionCable.server.broadcast 'notification_channel',  counter: render_counter(counter), notification: render_notification(notification)
   end
 
   private
 
   def render_counter(counter)
     ApplicationController.renderer.render(partial: 'notifications/counter', locals: { counter: counter })
+  end
+
+  def render_notification(notification)
+    ApplicationController.renderer.render(partial: 'notifications/notification', locals: { notification: notification })
   end
 end
